5c7644e2ec5f7b426555d3e216923b2457c9ecae
Racz Norbert <racznorbert766@gmail.com>
2026-07-24 22:18:48 +0200

    Created RandomTask

---
+++ src/test/java/RandomTaskTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import randomtask.RandomTask;
+
+public class RandomTaskTest {
+
+  @Test
+  void addTaskTest() {
+    RandomTask randomTask = new RandomTask();
+  }
+}
