a148545a302501745010eee6152f4bdd8efa7788
Racz Norbert <racznorbert766@gmail.com>
2026-07-26 12:02:04 +0200

    Removed webapp for rewrite

--- src/test/java/webapp/AdminTest.java
+++ src/test/java/webapp/AdminTest.java
@@ -1,45 +1,19 @@
 package webapp;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AdminTest {
-  static WebDriver driver;
-  static ConfigurableApplicationContext context;
-
-  @BeforeAll
-  static void setup() {
-    context = SpringApplication.run(Main.class, "--server.port=8081");
-    driver = new ChromeDriver();
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-  }
-
-  @AfterAll
-  static void teardown() {
-    driver.quit();
-    context.close();
-  }
 
   @Test
-  @DisplayName("When I log in as admin I should see the list of users")
-  void loginAsAdmin(){
+  void seleniumWorks(){
+    WebDriver driver = new ChromeDriver();
 
-    driver.get("http://localhost:8081");
-    driver.findElement(By.id("username")).sendKeys("admin");
-    driver.findElement(By.id("password")).sendKeys("admin");
-    driver.findElement(By.id("login-button")).click();
+    driver.get("https://www.google.com/");
 
-    assertTrue(driver.findElement(By.id("users-list")).isDisplayed());
+    assertTrue(true);
   }
 }
