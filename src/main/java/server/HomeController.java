037530aa04ac77eca0ac07a5a2b0100c0eb0d9dd
Racz Norbert <racznorbert766@gmail.com>
2026-07-26 21:58:51 +0200

    When the page loads I want to see an input for the username

---
+++ src/main/java/server/HomeController.java
@@ -0,0 +1,20 @@
+package server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+  @GetMapping("/")
+  public String home() {
+    return """
+            <!DOCTYPE html>
+            <html>
+              <body>
+                <input id="username"/>
+              </body>
+            </html>
+            """;
+  }
+}
--- src/test/java/e2e/UserStoriesTest.java
+++ src/test/java/e2e/UserStoriesTest.java
@@ -1,7 +1,10 @@
 package e2e;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.SpringApplication;
@@ -11,16 +14,29 @@ import server.Main;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserStoriesTest {
+  static ConfigurableApplicationContext context;
+  static WebDriver driver;
 
-  @Test
-  @DisplayName("Opening http://localhost:8081 I want a page to load")
-  public void userStory() {
-    ConfigurableApplicationContext context = SpringApplication.run(Main.class, "--server.port=8081");
-    WebDriver driver = new ChromeDriver();
-    driver.get("http://localhost:8081");
+  @BeforeAll
+  static void setup() {
+    context = SpringApplication.run(Main.class, "--server.port=8081");
+    driver = new ChromeDriver();
+  }
 
+  @AfterAll
+  static void teardown() {
     driver.quit();
     context.close();
-    assertTrue(true);
+  }
+
+  /*
+  Opening http://localhost:8081 I want a page to load
+   */
+  @Test
+  @DisplayName("When the page loads I want to see an input for the username")
+  void userStory() {
+    driver.get("http://localhost:8081");
+
+    assertTrue(driver.findElement(By.cssSelector("input#username")).isDisplayed());
   }
 }
